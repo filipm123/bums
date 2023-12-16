c94c0a19918bd897f43b44905c2e4b49a6ff9ba5
Filipor <filipor123@gmail.com>
2023-12-16 19:45:07 +0100

    ui fix

--- src/app/components/Note.js
+++ src/app/components/Note.js
@@ -128,7 +128,7 @@ const Note = ({ id }) => {
         ))}{" "}
       </div>
       <Button
-        className="mt-4 w-full"
+        className="mt-2 w-full"
         variant="outlined"
         onClick={handleOpen}
       >
