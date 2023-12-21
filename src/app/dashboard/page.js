39b06d248eac29368a4867d3c96d834f638c35e7
Filipor <filipor123@gmail.com>
2023-12-21 01:53:07 +0100

    chore

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -39,5 +39,5 @@ export default function DashboardPage() {
       </>
     );
   }
-  return <Loading />;
+  return <SignIn />;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,13 @@ import React, { useState, useEffect, useRef } from "react";
 import DOTS from "vanta/src/vanta.dots";
 import anime from "animejs/lib/anime.es.js";
 export default function Home() {
-  
   return (
-    <div
-     
-      className="flex flex-grow flex-col items-center justify-center gap-12 overflow-hidden text-center text-5xl"
-    >
-     
-      <div id='fade' className="px-4 text-base xl:text-4xl">
+    <div className="flex flex-grow flex-col items-center justify-center gap-12 overflow-hidden text-center text-5xl">
+      <div id="fade" className="px-4 text-base xl:text-4xl">
         <strong>BUMS: </strong> an easy way to manage your wip albums!
       </div>
       <Link href="/dashboard">
-        <Button id='fade' variant="outlined" size="large">
+        <Button id="fade" variant="outlined" size="large">
           Get started
         </Button>
       </Link>
